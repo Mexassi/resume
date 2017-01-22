b20aa5a9f1e1967dc3e34d4706720dc24be39c8a
Mexassi <mexassi@hotmail.it>
2017-01-22 21:52:47 +1100

    opens dialog with animation starting from event client position

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+var overlay = document.getElementById('overlay');
+var modalContainer = document.getElementById('modal-container');
+var dialogAnimationDuration = 250;
+
+function openModal(event){
+  modalContainer.style.top = event.clientY + 'px';
+  modalContainer.style.left = event.clientX + 'px';
+  overlay.classList.remove('is-hidden');
+  var removeStyleTimeout = setTimeout(function () {
+    modalContainer.style.top = 0;
+    modalContainer.style.left = 0;
+    clearTimeout(removeStyleTimeout);
+  }, dialogAnimationDuration);
+}
+
+function closeModal(){
+  overlay.classList.add('is-hidden');
+}
