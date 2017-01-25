752ef4e1f3ae4b89c7df396a79dea1f3f972c8a7
Mexassi <mexassi@hotmail.it>
2017-01-25 22:07:42 +1100

    use a promise to load partial views into the modal body and change its title. javascript content wrapped in IIFE using a revealing pattern.

---
+++ README.md
@@ -0,0 +1,14 @@
+## RESUME
+This is just a new version of my resume.
+
+I developed mexassi.github.io/resumeapp when I was still at uni and i figured It was time for an update.
+
+### Specifications
+I decided not to use any kind of css/javascript framework and to keep it as simple as possible. I work with different frameworks on my daily job so I guess I just wanted to show myself that I was still able to master the basic principles of web development.
+
+### Generator
+I spent so much time over the past two years on development/maintenance of building systems using gulp that I allowed myself to use a yeoman generator called Webapp to speed up the scaffolding process.
+
+Yeah, I know... this readme file is boring and useless. If you had the perseverance to read it to this point I feel you really deserve at least a thank you, and I hope that this will change your day in a positive way.
+
+Peace.
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,82 @@
-'use strict';
-var overlay = document.getElementById('overlay');
-var modalContainer = document.getElementById('modal-container');
-var dialogAnimationDuration = 250;
+var namespace = (function() {
+  'use strict';
 
-function openModal(event){
-  modalContainer.style.top = event.clientY + 'px';
-  modalContainer.style.left = event.clientX + 'px';
-  overlay.classList.remove('is-hidden');
-  var removeStyleTimeout = setTimeout(function () {
-    modalContainer.style.top = 0;
-    modalContainer.style.left = 0;
-    clearTimeout(removeStyleTimeout);
-  }, dialogAnimationDuration);
-}
+  var overlay = document.getElementById('overlay');
+  var dialogAnimationDuration = 250;
 
-function closeModal(){
-  overlay.classList.add('is-hidden');
-}
+  return {
+    openModal: openModal,
+    closeModal: closeModal,
+    loadTemplate: loadTemplate
+  };
+
+  function openModal(event, section) {
+    var modalContainer = document.getElementById('modal-container');
+    setModalPositionFrom(event, modalContainer);
+    self.overlay.classList.remove('is-hidden');
+
+    var removeStyleTimeout = setTimeout(function () {
+      unsetModalPositionFrom(modalContainer);
+      loadTemplate(section);
+      clearTimeout(removeStyleTimeout);
+    }, dialogAnimationDuration);
+  }
+
+  function closeModal(){
+    var modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML = '';
+    unsetTitle();
+    overlay.classList.add('is-hidden');
+  }
+
+  function loadTemplate(section) {
+    var modalBody = document.getElementById('modal-body');
+    var view = 'views/' + section + '.html';
+    getPartial(view).then(function(response) {
+      modalBody.innerHTML = response;
+      setTitle(section);
+    }, function (error) {
+      console.log(error)
+      modalBody.innerHTML = 'Ops! Could not fetch the content at this time! Sorry';
+    });
+  }
+
+  function setModalPositionFrom(event, container) {
+    container.style.top = event.clientY + 'px';
+    container.style.left = event.clientX + 'px';
+  }
+
+  function unsetModalPositionFrom(container) {
+    container.style.top = 0;
+    container.style.left = 0;
+  }
+
+  function setTitle(section) {
+    var title = document.getElementById('modal-title');
+    title.innerHTML = section.toUpperCase();
+  }
+
+  function unsetTitle() {
+    var title = document.getElementById('modal-title');
+    title.innerHTML = '';
+  }
+
+  function getPartial(view) {
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open('GET', view);
+      req.onload = function() {
+        if (req.status == 200) {
+          resolve(req.response);
+        }
+        else {
+          reject(Error(req.statusText));
+        }
+      };
+      req.onerror = function() {
+        reject(Error('Network Error'));
+      };
+      req.send();
+    });
+  }
+})();
